e0b745ef808f382eb7aef80c760e4805825c82cf
bang <michael.bangham3@gmail.com>
2022-09-20 14:52:02 +0100

    Delete build_exe directory

--- build_exe/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo -------------------------------------------
-echo Control-F 2022 - Binary Concat
-echo -------------------------------------------
-echo Due to Github file size restrictions, the executable for this program has been broken into chunks. This script will rebuild the executable from the chunks found in the build_exe directory (in order).
-echo -------------------------------------------
-COPY /B shomium.0 + shomium.1 + shomium.2 + shomium.3 + shomium.4 + shomium.5 + shomium.6 + shomium.7 + shomium.8 shomium.exe
-echo -------------------------------------------
-echo Finished!
